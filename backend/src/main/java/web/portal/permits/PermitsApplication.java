1b211cb16c4547413363c9b004f7b2f86d763c40
Bruno <broberti@ripple.com>
2022-01-18 11:09:39 +0000

    first commit

---
+++ backend/src/main/java/web/portal/permits/PermitsApplication.java
@@ -0,0 +1,13 @@
+package web.portal.permits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermitsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/web/portal/permits/PermitsApplicationTests.java
@@ -0,0 +1,13 @@
+package web.portal.permits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
